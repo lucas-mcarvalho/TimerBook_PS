cb68e4f2b2595c7a42e663eb1348d98c1754d6f5
TiagoBrs <barbosa.castro@mail.uft.edu.br>
2026-03-23 13:55:25 -0300

    Revert "Feature/tela home+janela adição livros"

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -4,12 +4,9 @@ function Home() {
   return (
     <div>
       <h1>TimerBook</h1>
-      <Link to="/TelaHome">Tela Home</Link>
-        <br />
       <Link to="/leitor">Ir para o leitor de PDF</Link>
         <br />
       <Link to="/cadastrar-livro">Cadastrar Livro</Link>
-        <br />
       <Link to="/meus-livros">Minha Biblioteca</Link>
       
     </div>
