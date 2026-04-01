94fa98a5553dddce35849db51324c36af5277548
Lucas Monteiro <lucasggtyy358@gmail.com>
2026-04-01 16:00:31 -0300

    Bug da reading duplicada corrijido

--- BackEnd/TimerBook_API/src/main/java/com/timerbook/TimerBook/repository/ReadingRepository.java
+++ BackEnd/TimerBook_API/src/main/java/com/timerbook/TimerBook/repository/ReadingRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReadingRepository extends JpaRepository<Reading, Long> {
     long countByFinishedAtIsNull();
     List<Reading> findByBookId(Long bookId);
+    Optional<Reading> findByBookIdAndFinishedAtIsNull(Long bookId);
 }
