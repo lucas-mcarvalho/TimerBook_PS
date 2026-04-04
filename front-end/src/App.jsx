fd6cf193cfe932fe8d5a60b09d7bfffdbb6e729d
Vítor Kawan <vitorbale8@gmail.com>
2026-04-04 19:20:55 -0300

    Estatiscas livros
    Estatísticas feitas para os livros, melhoramento e criação de botões auxiliares para as estatísticas de cada livro

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,6 +4,7 @@ import Leitor from "./pages/Leitor";
 import Cadastrar_Livro from "./pages/Cadastrar_Livro";
 import UserLibrary from "./pages/UserLibrary";
 import ReadingStatsPage from "./pages/ReadingStatsPage";
+import Estatisticas from "./pages/Estatisticas";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/cadastrar-livro" element={<Cadastrar_Livro />} />
         <Route path="/meus-livros" element={<UserLibrary />} />
         <Route path="/minhas-estatisticas" element={<ReadingStatsPage />} />
+        <Route path="/estatisticas/:readingId" element={<Estatisticas />} />
       </Routes>
     </BrowserRouter>  
   );
