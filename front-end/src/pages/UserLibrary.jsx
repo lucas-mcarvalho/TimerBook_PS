2a61496764845f02eadc2f3ab3a19116a36c1649
kayke02 <kaykezagopinheiro2020@gmail.com>
2026-05-17 13:21:28 -0300

    correção em ui

--- front-end/src/pages/UserLibrary.jsx
+++ front-end/src/pages/UserLibrary.jsx
@@ -5,7 +5,6 @@ import { getBookByUserId } from "../features/books/booksApi.js";
 import { useNavigate } from "react-router-dom";
 import { endReading, getReadingInProgressByBookId, startBookReadingSession } from "../features/books/readSessions.js";
 
-
 import '../styles/Layout.css';
 import '../styles/Library.css';
 import '../styles/Home.css';
@@ -24,6 +23,7 @@ function BookCard({ book, onRead, onDelete, isEditing, onOpenStats }) {
       {isEditing && (
         <button 
           className="btn-delete-book"
+          title="Excluir livro"
           onClick={(e) => { 
             e.preventDefault(); 
             e.stopPropagation();
@@ -32,7 +32,21 @@ function BookCard({ book, onRead, onDelete, isEditing, onOpenStats }) {
             }
           }}
         >
-          X
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="18" 
+            height="18" 
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
+          >
+            <path d="M3 6h18"></path>
+            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
+            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
+          </svg>
         </button>
       )}
 
@@ -85,8 +99,8 @@ function BookCard({ book, onRead, onDelete, isEditing, onOpenStats }) {
           }}
           className="btn-stats"
         >
-      Finalizar Leitura
-      </button>
+          Finalizar Leitura
+        </button>
       </div>
     </div>
   );
@@ -167,7 +181,8 @@ function UserLibrary() {
       setError("Erro ao iniciar leitura: " + err.message);
     }
   };
-   const handleOpenStats = async (readingId) => {
+  
+  const handleOpenStats = async (readingId) => {
      navigate(`/estatisticas/${readingId}`);
   };
 
